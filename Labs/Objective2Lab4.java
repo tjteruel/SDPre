42f16560ee90500345d2e184a346bd4573945d68
tjteruel <tjteruel@gmail.com>
2020-01-08 14:56:15 -0800

    complete Objective2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,15 @@
+public class Objective2Lab4 {
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+    double a, b;
+
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+
+    a = num1+num2+num3;
+    b = a / 3;
+  System.out.println("average is " + b );
+  }
+
+}
