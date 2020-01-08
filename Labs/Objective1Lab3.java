6cb47875a7626fe52fbe879ac4cccee983349fc9
tjteruel <tjteruel@gmail.com>
2020-01-08 13:26:32 -0800

    Complete Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+    System.out.println("Name: Tim Teruel");
+    System.out.println("Age: 30");
+    System.out.println("State: California");
+  }
+}
