4913735e0f99a0f45152fbd7a5ac5ab013bb4eb1
tjteruel <tjteruel@gmail.com>
2020-01-08 13:52:30 -0800

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name ="Tim Teruel";
+    int age = 30;
+    String state = "California";
+    System.out.println("Name: " + name + "Age: " + age + "State: " + state );
+  }
+}
