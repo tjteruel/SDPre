45a47defb1e0945c369c8931a966495fdf87f953
tjteruel <tjteruel@gmail.com>
2020-01-08 14:10:29 -0800

    complete Objective2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1, food2, food3;
+
+    food1 = "pizza";
+    food2 = "sushi";
+    food3 = "tacos";
+
+    System.out.println("My favorite foods are:");
+    System.out.println("1. " + food1);
+    System.out.println("2. " + food2);
+    System.out.println("3. " + food3);
+  }
+}
