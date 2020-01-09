365918879f12c8dc052244ffca29af0fb3e7cb84
tjteruel <59513125+tjteruel@users.noreply.github.com>
2020-01-09 13:52:56 -0800

    complete Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,15 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+  int count = 0;
+  int sum = 0;
+
+    while(count <= 20) {
+      System.out.println(sum);
+
+      count = count+1;
+
+      sum += count;
+
+    }
+  }
+}
